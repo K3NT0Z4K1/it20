f1eecaa7aba4370276c3467f6bb3d95d46dfcbb2
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2024-09-22 14:00:50 +0800

    added Scanner

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+package arraylist;
+
+import java.util.Scanner;
+public class Main {
+
+      public static void main(String[] args) {
+		
+          ItemManager list = new ItemManager();
+           
+           
+        
+        Scanner scan = new Scanner(System.in);
+            
+       
+                 
+                 
+  }
+            
+        
+}
