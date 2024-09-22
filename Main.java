f5a39d57f79373402c0a1252bf10fabbb4620ba5
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2024-09-22 14:02:22 +0800

    Added choices

--- Main.java
+++ Main.java
@@ -11,6 +11,16 @@ public class Main {
            
         
         Scanner scan = new Scanner(System.in);
+
+
+            System.out.println("Choose: ");
+            System.out.println("1. Insert an Item ");
+            System.out.println("2. Display all Item ");
+            System.out.println("3. Update an Item ");
+            System.out.println("4. Find an Item ");
+            System.out.println("5. Delete an Item ");
+            System.out.println("6. Exit ");
+  
             
        
                  
