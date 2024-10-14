e3df5e888c00a59a7c8ef580656d31910c91873d
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2024-10-14 14:54:59 +0800

    Customer

--- topic_4_queue/src/topic_4_queue/Customer.java
+++ topic_4_queue/src/topic_4_queue/Customer.java
@@ -11,6 +11,15 @@ package topic_4_queue;
  */
 public class Customer {
     
+    private String name;
 
+    public Customer(String name) {
+        this.name = name;
     }
+
+   
+}
+    
+
+    
 
