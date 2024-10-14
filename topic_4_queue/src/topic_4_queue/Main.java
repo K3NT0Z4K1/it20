b2255862def44de7a21f5d91cbad1ba8db33ad7b
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2024-10-14 14:48:35 +0800

    Initial Commit

---
+++ topic_4_queue/src/topic_4_queue/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topic_4_queue;
+
+/**
+ *
+ * @author ITLAB2-PC14-STUDENT
+ */
+public class Topic_4_queue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
