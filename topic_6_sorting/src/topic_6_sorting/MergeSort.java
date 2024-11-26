a60eb973be95a5a25caecf0f6df489166e684849
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2024-11-26 19:20:09 +0800

    Partial Modification Merge Sort

--- topic_6_sorting/src/topic_6_sorting/MergeSort.java
+++ topic_6_sorting/src/topic_6_sorting/MergeSort.java
@@ -1,5 +1,8 @@
 package topic_6_sorting;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MergeSort {
     public static void mergeSort(int[] arr) {
         if (arr.length < 2) {
@@ -38,21 +41,37 @@ public class MergeSort {
         }
     }
 
-    public static void printArray(int[] arr) {
-        for (int i : arr) {
-            System.out.print(i + " ");
+   public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> NumsToSort = new ArrayList<Integer>();
+        
+        System.out.println("Insertion Sorting ");
+
+        System.out.println("How many numbers you want to sort?");
+        int nums = scan.nextInt();
+        scan.nextLine();
+
+        for (int i = 1; i <= nums; i++) {
+
+            System.out.println("Enter the number you want to sort");
+            int num = scan.nextInt();
+            NumsToSort.add(num);
+
+            if (nums == i) {
+
+                System.out.println("Original ArrayList:");
+                System.out.println(NumsToSort);
+
+              mergeSort(NumsToSort);
+
+                System.out.println("Sorted ArrayList:");
+                System.out.println(NumsToSort);
+
+                break;
+            }
+
         }
-        System.out.println();
-    }
 
-    public static void main(String[] args) {
-        int[] arr = {38, 27, 43, 3, 9, 82, 10};
-        System.out.println("Original array:");
-        printArray(arr);
-
-        mergeSort(arr);
-
-        System.out.println("Sorted array:");
-        printArray(arr);
     }
 }
