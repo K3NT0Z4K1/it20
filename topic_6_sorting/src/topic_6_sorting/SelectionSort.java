28beb783650fc96a8106548ac865502fb50860c9
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2024-11-26 15:07:11 +0800

    Added scanner in bubble sort

--- topic_6_sorting/src/topic_6_sorting/SelectionSort.java
+++ topic_6_sorting/src/topic_6_sorting/SelectionSort.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package topic_6_sorting;
 
-/**
- *
- * @author ITLAB1-PC23-STUDENT
- */
 public class SelectionSort {
-    
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Original array:");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
 }
