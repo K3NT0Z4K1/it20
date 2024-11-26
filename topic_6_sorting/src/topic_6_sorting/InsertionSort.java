9d2e61129918d386ec15d68ce2fc39069c7b914c
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2024-11-26 19:16:50 +0800

    Fully Modified Insertion Sorting

--- topic_6_sorting/src/topic_6_sorting/InsertionSort.java
+++ topic_6_sorting/src/topic_6_sorting/InsertionSort.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InsertionSort {
-    public static void insertionSort(int[] arr) {
-        int n = arr.length;
+    public static void insertionSort(ArrayList<Integer> arr) {
+        int n = arr.size(); 
         for (int i = 1; i < n; i++) {
-            int key = arr[i];
+            int key = arr.get(i); 
             int j = i - 1;
 
-            // Move elements of arr[0..i-1], that are greater than key, to one position ahead
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
+            // Move elements of arr[0..i-1] that are greater than key to one position ahead
+            while (j >= 0 && arr.get(j) > key) {
+                arr.set(j + 1, arr.get(j));
                 j = j - 1;
             }
-            arr[j + 1] = key;
+            arr.set(j + 1, key); 
         }
     }
 
